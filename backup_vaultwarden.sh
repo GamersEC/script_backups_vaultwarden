ceca462a26acf9ab614c2f918413ea8a2e76d3ef
GamersEC <marcusalexander1999@gmail.com>
2026-02-11 17:20:30 -0500

    feat: codificar caracteres HTML en notificaciones de Telegram

--- backup_vaultwarden.sh
+++ backup_vaultwarden.sh
@@ -78,6 +78,10 @@ send_notif() {
     local message="<b>$status_icon $title</b>%0A%0A"
     message+="<b>Servidor:</b> <code>$HOST</code>%0A"
     message+="$body"
+    
+    # Codificar caracteres HTML para URL
+    message="${message//</%3C}"
+    message="${message//>/%3E}"
 
     if curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
          -d "chat_id=$CHAT_ID" \
